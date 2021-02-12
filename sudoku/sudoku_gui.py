3336b6b07fd704dec1d6b9f94646c5d3fa5ad028
Beat Schärz <beatschaerz@hotmail.com>
2021-02-12 10:59:49 +0100

    done :-)

--- sudoku/sudoku_gui.py
+++ sudoku/sudoku_gui.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 WIDTH, HEIGHT = 900, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -7,23 +8,40 @@ pygame.display.set_caption("Sudoku Solver")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-FPS = 60
+# increase for faster solving
+FPS = 6
 
-BOARD = [
-    [8,0,0,0,0,0,0,0,0],
-    [0,0,3,6,0,0,0,0,0],
-    [0,7,0,0,9,0,2,0,0],
-    [0,5,0,0,0,7,0,0,0],
-    [0,0,0,0,4,5,7,0,0],
-    [0,0,0,1,0,0,0,3,0],
-    [0,0,1,0,0,0,0,6,8],
-    [0,0,8,5,0,0,0,1,0],
-    [0,9,0,0,0,0,4,0,0]
+# hardest sodoku (increase FPS to > 200 or it takes forever)
+# or just comment out "clock.tick(FPS)"
+# board = [
+#     [8,0,0,0,0,0,0,0,0],
+#     [0,0,3,6,0,0,0,0,0],
+#     [0,7,0,0,9,0,2,0,0],
+#     [0,5,0,0,0,7,0,0,0],
+#     [0,0,0,0,4,5,7,0,0],
+#     [0,0,0,1,0,0,0,3,0],
+#     [0,0,1,0,0,0,0,6,8],
+#     [0,0,8,5,0,0,0,1,0],
+#     [0,9,0,0,0,0,4,0,0]
+# ]
+# easy sudoku (choose FPS as low as you want)
+board= [
+    [0,0,1,5,0,9,4,0,2],
+    [0,9,0,1,0,4,5,0,0],
+    [3,5,0,0,0,8,0,9,0],
+    [0,8,3,2,5,1,0,7,0],
+    [0,2,9,7,0,0,3,4,0],
+    [5,0,0,9,0,0,0,0,8],
+    [0,0,0,0,6,0,0,5,0],
+    [0,0,0,0,9,0,2,0,6],
+    [0,4,2,8,0,0,0,0,7]
 ]
 
 pygame.font.init()
 myfont = pygame.font.SysFont('Arial', 50)
+clock = pygame.time.Clock()
 
+# used to get the next empty field of sudoku
 def getNextEmpty(b):
     for rowi in range(len(b)):
         for coli in range(len(b[rowi])):
@@ -31,6 +49,7 @@ def getNextEmpty(b):
                 return (rowi, coli)
     return False
 
+# is the planned move valid?
 def isValidMove(b, pos, num):
     #check row
     for coli in range(len(b[pos[0]])):
@@ -49,6 +68,7 @@ def isValidMove(b, pos, num):
                 return False
     return True
 
+# draw and update the pygame sudoku board
 def draw_board(b):
     WIN.fill(BLACK)
     for i in range(9):
@@ -72,17 +92,36 @@ def draw_board(b):
             WIN.blit(text, (left+35, top+18))
     pygame.display.update()
 
-def main():
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
+# actual solving function that is called recursively
+def solve(b):
+    # PYGAME STUFF
+    clock.tick(FPS)
+    for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-        
-        draw_board(BOARD)
-    pygame.quit()
+                sys.exit(1)
+    draw_board(b)
+
+    # SOLVING
+    #get next empty space or done if none
+    nextEmpty = getNextEmpty(b)
+    if not nextEmpty:
+        return True
+    #try 0 to 9, if valid, enter it
+    for num in range(1,10):
+        if isValidMove(b, nextEmpty, num):
+            b[nextEmpty[0]][nextEmpty[1]] = num
+            #recursive call (try advancing with set number)
+            if solve(b):
+                return True
+            #if cant be solved, set changed pos back to 0
+            b[nextEmpty[0]][nextEmpty[1]] = 0
 
 if __name__ == "__main__":
-    main()
+    solve(board)
+    # this is only used to keep the window open when solving finishes
+    while True:
+        clock.tick(1)
+        draw_board(board)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(1)
